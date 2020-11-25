c299bd6241fec29a9b2b709afbbc7d24505a34f2
vishnuroshan <vishnuroshan4@gmail.com>
2020-11-25 19:04:19 +0530

    accordian

--- src/components/Dashboard/Url/Url.js
+++ src/components/Dashboard/Url/Url.js
@@ -1,42 +1,60 @@
-import React from 'react'; import { makeStyles } from '@material-ui/core/styles';
-import { Card, Typography, Link, CardContent, ListItem } from "@material-ui/core";
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Card, Typography, Link, CardContent, ListItem, Accordion, AccordionDetails, AccordionSummary } from "@material-ui/core";
+import { ExpandMore, ExpandLess } from '@material-ui/icons'
 // import useClipboard from "react-use-clipboard";
 
-
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%'
+    },
+    heading: {
+        fontSize: theme.typography.pxToRem(15),
+        flexBasis: '33.33%',
+        flexShrink: 0
+    },
+    secondaryHeading: {
+        fontSize: theme.typography.pxToRem(15),
+        color: theme.palette.text.secondary
+    }
+}));
 
 function UrlComponent(props) {
 
-    // const [isCopied, setCopied] = useClipboard("sasdasd");
-
-    const useStyles = makeStyles({
-        root: {
-            minWidth: 275,
-        },
-        bullet: {
-            display: 'inline-block',
-            margin: '0 2px',
-            transform: 'scale(0.8)',
-        },
-        title: {
-            fontSize: 14,
-        },
-        pos: {
-            marginBottom: 12,
-        },
-    });
     const classes = useStyles();
+    const [expanded, setExpanded] = useState(false);
 
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
     return (
-        <div className="card">
-            <div className="card-body">
-                <h5 className="card-title">{props.uniqueKey}</h5>
-                <p className="card-text">{props.url}</p>
-                <Link href={`${process.env.REACT_APP_API_URL}${props.uniqueKey}`} className="card-link">
-                    {`${process.env.REACT_APP_API_URL}${props.uniqueKey}`}
+        // <div className="card">
+        //     <div className="card-body">
+        //         <h5 className="card-title">{props.uniqueKey}</h5>
+        //         <p className="card-text">{props.url}</p>
+        //         <Link href={`${process.env.REACT_APP_API_URL}${props.uniqueKey}`} className="card-link">
+        //             {`${process.env.REACT_APP_API_URL}${props.uniqueKey}`}
+        //         </Link>
+        //         {/* <button className="btn btn-outline-primary" onClick={setCopied(`${process.env.REACT_APP_API_URL}${props.uniqueKey}`)}>copy to clipboard</button> */}
+        //     </div>
+        // </div>
+
+        <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+            <AccordionSummary
+                expandIcon={<ExpandMore />}
+                aria-controls="panel1bh-content"
+                id="panel1bh-header"
+            >
+                <Link
+                    href={`${process.env.REACT_APP_API_URL}${props.uniqueKey}`}
+                    className={classes.heading}>{`${process.env.REACT_APP_API_URL}${props.uniqueKey}`}
                 </Link>
-                {/* <button className="btn btn-outline-primary" onClick={setCopied(`${process.env.REACT_APP_API_URL}${props.uniqueKey}`)}>copy to clipboard</button> */}
-            </div>
-        </div>
+                <Typography className={classes.secondaryHeading}>{props.url}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                clicks: {props.clicks}
+            </AccordionDetails>
+        </Accordion>
 
         // <Card classNameName={classNamees.root}>
         //     <CardContent>
